dc61a49741141e3f66940271e1240aa345798e8e
ASTIK <astik.ytt@gmail.com>
2024-04-03 22:11:18 +0200

    Refactor: Move file extension changing logic into compressImage() function

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 import path from 'path';
-import fs from 'fs';
 import { glob } from 'glob';
 
 import compressImage from './lib/compressImage.js';
@@ -9,30 +8,13 @@ const originalsFolder = "./images/originals/";
 const compressedFolder = "./images/compressed/";
 
 
-function changeFileExtension (filePath, newExtension) {
-    const filePathWithoutExtension = filePath.slice(0, -path.extname(filePath).length);
-    return filePathWithoutExtension + "." + newExtension;
-}
-
-
 const originalImgs = await glob('./**/*.{jpg,jpeg,png}', {cwd: originalsFolder});
 
-if (originalImgs.length > 0) {
-    fs.mkdirSync(compressedFolder, { recursive: true });
-} else {
-    console.log('No images found in', originalsFolder);
-}
-
 for (const originalImg of originalImgs) {
+    
+    const inputPath = path.join(originalsFolder, originalImg);
+    const outputPath = path.join(compressedFolder, originalImg);
 
-    const compressedImgPath = path.join(compressedFolder, originalImg);
-    const originalImgPath = path.join(originalsFolder, originalImg);
-
-    const compressedJpgImgPath = changeFileExtension(compressedImgPath, 'jpg');
-    const compressedWebpImgPath = changeFileExtension(compressedImgPath, 'webp');
-
-    fs.mkdirSync(path.dirname(compressedImgPath), { recursive: true });
-
-    compressImage(originalImgPath, compressedJpgImgPath, "jpeg");
-    compressImage(originalImgPath, compressedWebpImgPath, "webp");
+    compressImage(inputPath, outputPath, "jpeg");
+    compressImage(inputPath, outputPath, "webp");
 }
