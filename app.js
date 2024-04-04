ca2617c85ac8d7049434fb13616228264ea2ca4a
ASTIK <astik.ytt@gmail.com>
2024-04-04 17:00:07 +0200

    Refactor: Optimize directory creation

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import path from 'path';
+import { promises as fs } from 'fs';
 import { glob } from 'glob';
 
+import removeIntermediatePaths from './lib/removeIntermediatePaths.js';
 import compressImage from './lib/compressImage.js';
 
 
@@ -10,6 +12,19 @@ const compressedFolder = "./images/compressed/";
 
 const originalImgs = await glob('./**/*.{jpg,jpeg,png}', {cwd: originalsFolder});
 
+
+// Create a directory structure in the 'compressedFolder'
+// mirroring the structure of original images
+const uniqueDirectories = new Set(originalImgs.map(img => path.dirname(img)));
+const directoriesToMake = removeIntermediatePaths([...uniqueDirectories]);
+await Promise.all(
+    directoriesToMake.map(dir =>
+        fs.mkdir(path.join(compressedFolder, dir), { recursive: true })
+    )
+);
+
+
+// Compress each original image
 for (const originalImg of originalImgs) {
     
     const inputPath = path.join(originalsFolder, originalImg);
