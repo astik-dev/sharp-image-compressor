9ebcb5d0bc4ec28ad6f2e9c7d4dab46e95a722c0
ASTIK <astik.ytt@gmail.com>
2024-04-04 21:50:46 +0200

    Add functionality to prevent re-compression of compressed images

--- app.js
+++ app.js
@@ -3,14 +3,19 @@ import { promises as fs } from 'fs';
 import { glob } from 'glob';
 
 import removeIntermediatePaths from './lib/removeIntermediatePaths.js';
+import changeFileExtension from './lib/changeFileExtension.js';
 import compressImage from './lib/compressImage.js';
 
 
 const originalsFolder = "./images/originals/";
 const compressedFolder = "./images/compressed/";
+const compressionToFormats = ["jpeg", "webp"];
 
 
 const originalImgs = await glob('./**/*.{jpg,jpeg,png}', {cwd: originalsFolder});
+const compressedImgs = new Set(
+    await glob(`${compressedFolder}**/*.{${compressionToFormats.join(",")}}`)
+);
 
 
 // Create a directory structure in the 'compressedFolder'
@@ -30,6 +35,9 @@ for (const originalImg of originalImgs) {
     const inputPath = path.join(originalsFolder, originalImg);
     const outputPath = path.join(compressedFolder, originalImg);
 
-    compressImage(inputPath, outputPath, "jpeg");
-    compressImage(inputPath, outputPath, "webp");
+    compressionToFormats.forEach(format => {
+        const outputPathWithNewFormat = changeFileExtension(outputPath, format);
+        if (compressedImgs.has(outputPathWithNewFormat)) return;
+        compressImage(inputPath, outputPathWithNewFormat, format);
+    });
 }
