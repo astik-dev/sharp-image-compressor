1e969c380fcfff6b2e41512131be36d2be32de46
ASTIK <astik.ytt@gmail.com>
2024-04-06 20:30:58 +0200

    Refactor image compression options into 'config.js' file

--- config.js
+++ config.js
@@ -4,3 +4,23 @@ export const originalsFolder = "./images/originals/";
 export const compressedFolder = "./images/compressed/";
 // Formats to which original images will be compressed
 export const compressionToFormats = ["jpeg", "webp"];
+
+// Compression options for different formats
+// These options can be configured based on Sharp documentation for each format:
+// https://sharp.pixelplumbing.com/api-output
+export const compressionOptions = {
+    jpeg: {
+        quality: 80,
+        progressive: true,
+        mozjpeg: true,
+    },
+    png: {
+        quality: 80,
+    },
+    webp: {
+        quality: 70,
+    },
+    avif: {
+        quality: 70,
+    }
+}
